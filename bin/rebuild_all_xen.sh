66dff2ce80b51f67915cf3f8d01a22fd95425b69
Sean Grove <sean@bushi.do>
2016-03-01 11:22:31 -0800

    Trying not to explode with fury

--- bin/rebuild_all_xen.sh
+++ bin/rebuild_all_xen.sh
@@ -4,9 +4,11 @@ if [ -f _build/sanitize.sh ]; then
 fi
 echo "Cleaning"
 make clean
+echo "First make"
+make
 echo "Building client"
 make client
 echo "Configuring unikernel"
 env FS=crunch mirage configure --xen --dhcp=true --no-argv
-echo "Building unikernel"
+echo "Building unikernel via final make"
 make
