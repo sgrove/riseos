4becdc839fd7413806a856e6ef900f14c276fdcd
Sean Grove <sean@bushi.do>
2016-03-01 11:23:47 -0800

    Include ports for xen build

--- bin/rebuild_all_xen.sh
+++ bin/rebuild_all_xen.sh
@@ -9,6 +9,6 @@ make
 echo "Building client"
 make client
 echo "Configuring unikernel"
-env FS=crunch mirage configure --xen --dhcp=true --no-argv
+env FS=crunch mirage configure --xen --dhcp=true --no-argv --http_port=80 --https_port=443
 echo "Building unikernel via final make"
 make
