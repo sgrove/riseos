5e980eb9eff3341b6c2811116fb570162370e749
Sean Grove <sean@bushi.do>
2016-08-27 19:44:03 -0700

    Update CircleCI stuff

--- circle/install-ocaml.sh
+++ circle/install-ocaml.sh
@@ -1,10 +1,14 @@
 set -x
 set -e
 
-if [ ! -e opam ]; then
+if [ ! -e /home/ubuntu/bin/opam ]; then
+    sudo apt-get update
+    sudo apt-get install aspcud
     # This will simply check your architecture, download and install the proper pre-compiled binary and run opam init.
+    export PATH=$PATH:/home/ubuntu/bin/
+    export BINDIR=/home/ubuntu/bin
     wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
 
     opam switch -A 4.02.3 riseos
-    opam switch import opam/riseos.opam.snapshot
+    opam switch import /home/ubuntu/riseos/opam/riseos.opam.snapshot
 fi
--- circle/install-syslinux.sh
+++ circle/install-syslinux.sh
@@ -1,6 +1,8 @@
 set -x
 set -e
 if [ ! -e /home/ubuntu/syslinux/syslinux ]; then
+    sudo apt-get update
+    sudo apt-get install nasm
     wget https://www.kernel.org/pub/linux/utils/boot/syslinux/6.xx/syslinux-6.03.tar.gz
     tar -xzvf syslinux-6.03.tar.gz
     cd syslinux-6.03
