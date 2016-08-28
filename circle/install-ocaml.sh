2a9eb5ac53dd18dff2a1147db800825ed18f02d3
Sean Grove <sean@bushi.do>
2016-08-28 10:34:49 -0700

    Sigh

--- circle/install-ocaml.sh
+++ circle/install-ocaml.sh
@@ -10,5 +10,8 @@ if [ ! -e /home/ubuntu/bin/opam ]; then
     wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
 
     opam switch -A 4.02.3 riseos
-    opam switch import -y /home/ubuntu/riseos/opam/riseos.opam.snapshot
+    opam remote add mirage-dev https://github.com/mirage/mirage-dev.git
+    opam pin add mirage https://github.com/mirage/mirage.git#9be9b6160842d8f25a4c763fb6b6a0d8a4362631
+    opam pin add -y mirage-entropy-solo5 https://github.com/mirage/mirage-entropy.git
+    # opam switch import -y /home/ubuntu/riseos/opam/riseos.opam.snapshot
 fi
--- circle/install-syslinux.sh
+++ circle/install-syslinux.sh
@@ -1,13 +1,14 @@
 set -x
 set -e
-if [ ! -e /home/ubuntu/syslinux/syslinux ]; then
+if [ ! -e /home/ubuntu/bin/syslinux ]; then
     sudo apt-get update
     sudo apt-get install nasm
+    cd ~/
     wget https://www.kernel.org/pub/linux/utils/boot/syslinux/6.xx/syslinux-6.03.tar.gz
     tar -xzvf syslinux-6.03.tar.gz
     cd syslinux-6.03
     make
     sudo make install
     mkdir /home/ubuntu/syslinux
-    cp /usr/bin/syslinux /home/ubuntu/syslinux/
+    cp /usr/bin/syslinux /home/ubuntu/bin/
 fi
